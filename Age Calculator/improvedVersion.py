05ca435cdbbe1390bf6f9002d1c6b34a3447224b
unknown <cyver.peterson@gmail.com>
2025-12-29 22:36:50 +0800

    updated the bug on computing the number of days

--- Age Calculator/improvedVersion.py
+++ Age Calculator/improvedVersion.py
@@ -13,26 +13,49 @@ def isLeapYear(year) :
     return isleap(year)
 
 def getNumberOfDays() :
+    birth_year = int(splitBday[0])
+    birth_month = int(splitBday[1])
+    birth_day = int(splitBday[2])
+    
     days = 0
-    #get days on this year
-    for m in range(1, time.localtime.tm_mon) :
-        if m == 2 :
-            if isLeapYear(yearToday) :
-                days += 29
-            else :
-                days += 28
-        elif m in [1, 3, 5, 7, 8, 19, 12] : 
-            days += 31
-        else :
-            days += 30
-
-    for yr in range(int(splitBday[0]) + 1, yearToday) :
-        #print(yr)
-        if isLeapYear(yr) :
-            days += 366
-        else :
-            days += 365
+    
+    if birth_year == yearToday:
+        if birth_month == monthToday:
+            days = dayToday - birth_day
+        else:
+            for m in range(birth_month, monthToday):
+                if m == birth_month:
+                    days += getDays(m, birth_year) - birth_day
+                else:
+                    days += getDays(m, birth_year)
+            days += dayToday
+    else:
+        # Days from birth to end of birth year
+        days += getDays(birth_month, birth_year) - birth_day
+        for m in range(birth_month + 1, 13):
+            days += getDays(m, birth_year)
+        
+        # Full days in years between
+        for yr in range(birth_year + 1, yearToday):
+            days += 365 + (1 if isLeapYear(yr) else 0)
+        
+        # Days in current year from start to today
+        for m in range(1, monthToday):
+            days += getDays(m, yearToday)
+        days += dayToday
+    
     return days
+
+def getDays(m, year):
+    if m == 2 :
+        if isLeapYear(year) :
+            return 29
+        else :
+            return 28
+    elif m in [1, 3, 5, 7, 8, 10, 12] :
+        return 31
+    else : 
+        return 30
 
 def getNumberOfMonths() :
     firstMonth = 12 - int(splitBday[1])
